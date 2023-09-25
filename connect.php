7cd64bb4549be9789538e4cb13d833e3f6eeff2b
Hylke Storteboom <hylke@sterc.nl>
2023-09-25 14:10:08 +0200

    cleanup connection

--- connect.php
+++ connect.php
@@ -4,13 +4,6 @@ $username = "root";
 $password = "root";
 $dbname = "docs";
 
-// Form variables
-$title = $_POST['title'];
-$desc = $_POST['description'];
-$content = $_POST['content'];
-$author = $_POST['author'];
-
-
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
