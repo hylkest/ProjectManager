2e17006b88bdd3e3bdede63e63dfacbfc71cb598
Hylke Storteboom <hylke@sterc.nl>
2023-09-25 13:39:07 +0200

    usage of basepath

--- header.php
+++ header.php
@@ -3,6 +3,8 @@ session_start();
 if ($_SESSION['isadmin'] === "1" ) {
     echo "<h3>Admin User</h3>";
 }
+
+$basepath = "/projectmanager/"
 ?>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
@@ -17,7 +19,7 @@ if ($_SESSION['isadmin'] === "1" ) {
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="/projectmanager/index.php">Home</a>
+                    <a class="nav-link active" aria-current="page" href=<?php echo $basepath."index.php" ?>>Home</a>
                 </li>
                 <?php
 
@@ -29,15 +31,15 @@ if ($_SESSION['isadmin'] === "1" ) {
 
                 if ($_SESSION['loggedin'] === "1" ) {
                     echo "<li class='nav-item'>
-                    <a class='nav-link' href='/projectmanager/list.php'>List</a>
+                    <a class='nav-link' href='".$basepath."list.php'>List</a>
                 </li><li class='nav-item'>
-                    <a class='nav-link' href='/projectmanager/logout.php'>Logout</a>
+                    <a class='nav-link' href='".$basepath."logout.php'>Logout</a>
                 </li><li class='nav-item'>
-                <a class='nav-link' href='/projectmanager/settings.php'>Profile settings</a>
+                <a class='nav-link' href='".$basepath."settings.php'>Profile settings</a>
                 </li>";
                 } else {
                     echo "<li class='nav-item'>
-                    <a class='nav-link' href='/projectmanager/login.php'>Log in</a>
+                    <a class='nav-link' href='".$basepath."login.php'>Log in</a>
                 </li>";
                 }
                 ?>
